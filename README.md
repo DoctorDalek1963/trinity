79cc179755dc5c21a32d57030378839668c0a52f
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-08-22 16:36:52 +0100

    Create new project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Trinity
+
+Trinity is a program built to visualise and interact with matrices in the form of linear transformations. It is effectively a sequel to my earlier project, [lintrans](https://github.com/DoctorDalek1963/lintrans), but built to be faster, more robust, and most importantly, to work in 3D.
---
+++ flake.nix
@@ -0,0 +1,117 @@
+{
+  description = "Trinity, a program to visualise and interact with matrices";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.pre-commit-hooks.flakeModule
+      ];
+
+      systems = ["x86_64-linux" "aarch64-linux"];
+      perSystem = {
+        config,
+        system,
+        ...
+      }: let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [(import inputs.rust-overlay)];
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+
+        craneLib = (inputs.crane.mkLib pkgs).overrideToolchain rustToolchain;
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+      in rec {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            (rustToolchain.override {
+              extensions = ["rust-analyzer" "rust-src" "rust-std"];
+            })
+          ];
+          shellHook = ''
+            ${config.pre-commit.installationScript}
+          '';
+        };
+
+        # See https://flake.parts/options/pre-commit-hooks-nix and
+        # https://github.com/cachix/git-hooks.nix/blob/master/modules/hooks.nix
+        # for all the available hooks and options
+        pre-commit.settings.hooks = {
+          check-added-large-files.enable = true;
+          check-merge-conflicts.enable = true;
+          check-toml.enable = true;
+          check-vcs-permalinks.enable = true;
+          check-yaml.enable = true;
+          end-of-file-fixer.enable = true;
+          trim-trailing-whitespace.enable = true;
+
+          rustfmt = {
+            enable = true;
+            packageOverrides = {
+              cargo = rustToolchain;
+              rustfmt = rustToolchain;
+            };
+          };
+        };
+
+        checks = {
+          inherit (packages) trinity doc;
+
+          clippy = craneLib.cargoClippy (commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            });
+
+          fmt = craneLib.cargoFmt {
+            inherit src;
+          };
+        };
+
+        packages = rec {
+          default = trinity;
+
+          trinity = craneLib.buildPackage (commonArgs
+            // {
+              pname = "trinity";
+              inherit cargoArtifacts;
+              inherit (craneLib.crateNameFromCargoToml {inherit src;}) version;
+            });
+
+          doc = craneLib.cargoDoc (commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoDocExtraArgs = "--no-deps --document-private-items";
+              RUSTDOCFLAGS = "--deny warnings";
+            });
+        };
+      };
+    };
+}
