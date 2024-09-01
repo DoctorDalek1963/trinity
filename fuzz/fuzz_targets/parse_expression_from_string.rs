61877ac68c70964d911e464ffc04a318b0b3a32d
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-09-01 01:08:28 +0100

    Fuzz test parse_expression_from_string

---
+++ fuzz/fuzz_targets/parse_expression_from_string.rs
@@ -0,0 +1,9 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    if let Ok(s) = std::str::from_utf8(data) {
+        let _ = trinity::matrix::expression::parse_expression_from_string(s);
+    }
+});
